145128d55971c236531c3e9a6d3e2ff3a11664d1
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-02-22 16:46:08 +0530

    grid updated

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let animals = ['camel','cheetah','elephant','gorilla','koala','lion','polar_bear','tiger','wolf']
+
+let photoGrid = document.querySelector('.grid')
+
+for (let i = 0; i < animals.length; i++) {
+
+    console.log(animals[i]);
+    let animal = `<img src="https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animals[i]}" alt="${animals[i]}">`
+
+   photoGrid.innerHTML += animal;
+
+}
