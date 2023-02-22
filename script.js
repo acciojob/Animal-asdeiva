a5f66758e1a60dfc2ed003dbdd2e04742b2bce1e
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-02-22 16:49:27 +0530

    jpg added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let photoGrid = document.querySelector('.grid')
 for (let i = 0; i < animals.length; i++) {
 
     console.log(animals[i]);
-    let animal = `<img src="https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animals[i]}" alt="${animals[i]}">`
+    let animal = `<img src="https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animals[i]}.jpg" alt="${animals[i]}">`
 
    photoGrid.innerHTML += animal;
 
